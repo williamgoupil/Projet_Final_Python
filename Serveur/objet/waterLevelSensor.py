386d7878a8e16f5343bcd7907311cf718d00ad0b
Edlx <81708400+FrederickPerazzelli@users.noreply.github.com>
2022-05-10 13:21:17 -0400

    Update waterLevelSensor.py

--- Serveur/objet/waterLevelSensor.py
+++ Serveur/objet/waterLevelSensor.py
@@ -6,32 +6,23 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 class WaterLevelSensor:
-    def __init__(self, waterLeveSensor_pin):
-        self.waterLeveSensor_pin = waterLeveSensor_pin
-        GPIO.setup(self.waterLeveSensor_pin, GPIO.IN)
+    def __init__(self, waterLevel):
+        self.waterLevel = waterLevel
+
 
     def getWaterLevel(self, inputSelect, adc):
         return adc.analogRead(inputSelect)
         
-    def test(self):
-        value = self.getWaterLevel()
-        print(value)
-        time.sleep(1)
+    def setWaterLevel(self, waterLevel):
+        self.waterLevel = waterLevel
+            
+            
+    def getWaterLevel(self, waterLevel):
+        if waterLevel > 0 
+        
+        
+        
+        #return adc.analogRead(inputSelect)
 
-    def returnWaterLevel(self):
-
-        value = self.getWaterLevel()
-
-        #selon la value qu'on recoie retourne un % du niveau de l'eau
-
-        if value < 100:
-            return '10%'
-        elif value > 100 and value < 200:
-            return '20%'
-        else :
-            return '100%'
-
-        #Faire le if else lorsqu'on connais les value que retourne le waterLevel
-    
-    def destroy(self):
-        GPIO.cleanup()
+    def print(self):
+        print(self.waterLevel)
